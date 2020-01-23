bcaf169837d484d0bcf8686c18dc0529d1469034
xames3 <xames3.developer@gmail.com>
2020-01-24 00:47:23 +0530

    Restore stock install settings

--- mle_django/mle_django/urls.py
+++ mle_django/mle_django/urls.py
@@ -1,9 +1,8 @@
 """mle_django URL Configuration"""
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('beta/', include('beta.urls'),
-    path('admin/', admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
